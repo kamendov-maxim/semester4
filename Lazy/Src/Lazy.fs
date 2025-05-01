bc77abb442994d3ad7c02f4b9e5a5333e690545a
kamendov-maxim <kamendov.maksim@icloud.com>
2025-05-01 22:53:38 +0300

    add Lazy task

---
+++ Lazy/Src/Lazy.fs
@@ -0,0 +1,67 @@
+module Lazy
+
+type ILazy<'a> =
+    abstract member Get : unit -> 'a
+
+type SimpleLazy<'a>(supplier : unit -> 'a) =
+    let mutable result : Result<'a, exn> option = None
+    interface ILazy<'a> with
+        member _.Get() =
+            match result with
+            | Some (Ok value) -> value
+            | Some (Error ex) -> raise ex
+            | None ->
+                try
+                    let computed = supplier()
+                    result <- Some (Ok computed)
+                    computed
+                with ex ->
+                    result <- Some (Error ex)
+                    raise ex
+
+type ConcurrentLazy<'a>(supplier : unit -> 'a) =
+    let mutable result : Result<'a, exn> option = None
+    let lockObj = obj()
+    interface ILazy<'a> with
+        member _.Get() =
+            match result with
+            | Some (Ok value) -> value
+            | Some (Error ex) -> raise ex
+            | None ->
+                lock lockObj (fun () ->
+                    match result with
+                    | Some (Ok value) -> value
+                    | Some (Error ex) -> raise ex
+                    | None ->
+                        try
+                            let computed = supplier()
+                            result <- Some (Ok computed)
+                            computed
+                        with ex ->
+                            result <- Some (Error ex)
+                            raise ex
+                )
+
+type LockFreeLazy<'a>(supplier : unit -> 'a) =
+    let mutable result : Result<'a, exn> option = None
+    interface ILazy<'a> with
+        member _.Get() =
+            match result with
+            | Some res ->
+                match res with
+                | Ok value -> value
+                | Error ex -> raise ex
+            | None ->
+                let computedResult =
+                    try Ok (supplier()) 
+                    with ex -> Error ex
+                let current = System.Threading.Interlocked.CompareExchange(&result, Some computedResult, None)
+                match current with
+                | Some res ->
+                    match res with
+                    | Ok value -> value
+                    | Error ex -> raise ex
+                | None ->
+                    match computedResult with
+                    | Ok value -> value
+                    | Error ex -> raise ex
